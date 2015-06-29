20f56c655849926ffe744ee6eb51dcbf92aa2d1a
vic <haijian.zhou@hknetworktv.com>
2015-06-29 17:27:47 +0800

    enable the post and put

--- app/api/v1/rest/commodity_api.rb
+++ app/api/v1/rest/commodity_api.rb
@@ -19,29 +19,42 @@ module V1
               present Commodity.find(params[:id]), with: Entities::Commodity
             end
 
-            # desc 'Create a new commodity'
-            # params do
-            #   requires :all, except: [:id], using: Entities::Commodity.documentation.except(:id)
-            # end
-            # post do
-            #   commodity = Commodity.create! params
-            #   present commodity, with: Entities::Commodity
-            # end
+            desc 'Create a new commodity'
+            params do
+              requires :title, type: String, desc: "title"
+              requires :price, type: Float, desc: "price"
+              requires :cover, type: String, desc: "cover"
+              optional :summary, type: String, desc: "summary"
+            end
+            post do
+              commodity = Commodity.create!({
+                title: params[:title],
+                price: params[:price],
+                cover: params[:cover],
+                summary: params[:summary]
+              })
+              present commodity, with: Entities::Commodity
+            end
 
 
-            # desc 'Update an existing commodity'
-            # params do
-            #   requires :id, type: String, desc: 'Commodity id'
-            #   group :commodity do
-            #     optional :price, type: Float, desc: 'Commodity price'
-            #     optional :summary, type: String,  desc: 'Commodiy des'
-            #   end
-            # end
-            # put ':id' do
-            #   commodity = Commodity.find params[:id]
-            #   commodity.update_attributes parameters.require(:member).permit(:price, :summary)
-            #   present commodity, with: Entities::Commodity
-            # end
+            desc 'Update an existing commodity'
+            params do
+              requires :id, type: String, desc: "id"
+              optional :title, type: String, desc: "title"
+              optional :price, type: Float, desc: "price"
+              optional :cover, type: String, desc: "cover"
+              optional :summary, type: String, desc: "summary"
+            end
+            put ':id' do
+              commodity = Commodity.find params[:id]
+              commodity.update({
+                title: (params[:title]).blank? ? commodity.title : params[:title],
+                price: (params[:price].blank?) ? commodity.price : params[:price],
+                cover: (params[:cover].blank?) ? commodity.cover : params[:cover],
+                summary: (params[:summary].blank?) ? commodity.summary : params[:summary]
+              })
+              present commodity, with: Entities::Commodity
+            end
 
             desc 'Destroy a commodity'
             params do
